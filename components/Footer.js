8a65c1c330240c07b4a8d8d11afb73b847f0f262
maxthestranger <thestrangermax@gmail.com>
2021-11-15 00:34:27 +0300

    feature: footer component structure

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import { links } from '../data/footerData';
+
+export default function Footer() {
+  return (
+    <footer className="">
+      {links.map((link, index) => {
+        <a key={index} href={link.url} target="_blank" rel="noreferrer">
+          <span>{link.title}</span>
+          <i className={link.icon} />
+        </a>;
+      })}
+    </footer>
+  );
+}
