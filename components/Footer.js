b800e4d9ac283f1e1df8b657c8c84ef451e4b8d9
maxthestranger <thestrangermax@gmail.com>
2021-11-17 08:35:46 +0300

    feat: components design

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,38 @@
-import { links } from '../data/footerData';
-
 export default function Footer() {
-  return (
-    <footer className="footer">
-      {links.map((link, index) => (
-        <a key={index} href={link.url} target="_blank" rel="noreferrer">
-          <span>{link.title}</span>
-          <i className={link.icon} />
-        </a>
-      ))}
-    </footer>
-  );
+  const links = [
+    {
+      title: 'Source',
+      url: 'https://github.com/zenorocha/zenorocha.com',
+      icon: 'ri-braces-line'
+    },
+    {
+      title: 'Twitter',
+      url: 'https://twitter.com/zenorocha',
+      icon: 'ri-twitter-line'
+    },
+    {
+      title: 'GitHub',
+      url: 'https://github.com/zenorocha',
+      icon: 'ri-github-line'
+    },
+    {
+      title: 'linkedin',
+      url: 'https://linkedin.com/in/zenorocha',
+      icon: 'ri-linkedin-line'
+    },
+    {
+      title: 'Instagram',
+      url: 'https://instagram.com/zenorocha',
+      icon: 'ri-instagram-line'
+    },
+  ]
+
+  return <footer className="footer">
+    {links.map((link, index) => {
+      return <a key={index} href={link.url} target="_blank">
+        <span>{link.title}</span>
+        <i className={link.icon} />
+      </a>
+    })}
+  </footer>
 }
