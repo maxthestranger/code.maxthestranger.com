8d8c89d6805b3b5621aae9b75e1c030807dc0877
maxthestranger <thestrangermax@gmail.com>
2021-11-17 08:48:14 +0300

    added: footer links modification

--- components/Footer.js
+++ components/Footer.js
@@ -2,37 +2,41 @@ export default function Footer() {
   const links = [
     {
       title: 'Source',
-      url: 'https://github.com/zenorocha/zenorocha.com',
-      icon: 'ri-braces-line'
+      url: 'https://github.com/maxthestranger/maxthestranger.com',
+      icon: 'ri-braces-line',
     },
     {
       title: 'Twitter',
-      url: 'https://twitter.com/zenorocha',
-      icon: 'ri-twitter-line'
+      url: 'https://twitter.com/maxthestranger',
+      icon: 'ri-twitter-line',
     },
     {
       title: 'GitHub',
-      url: 'https://github.com/zenorocha',
-      icon: 'ri-github-line'
+      url: 'https://github.com/maxthestranger',
+      icon: 'ri-github-line',
     },
     {
       title: 'linkedin',
-      url: 'https://linkedin.com/in/zenorocha',
-      icon: 'ri-linkedin-line'
+      url: 'https://linkedin.com/in/maxthestranger',
+      icon: 'ri-linkedin-line',
     },
     {
       title: 'Instagram',
-      url: 'https://instagram.com/zenorocha',
-      icon: 'ri-instagram-line'
+      url: 'https://www.instagram.com/max_the_stranger/',
+      icon: 'ri-instagram-line',
     },
-  ]
+  ];
 
-  return <footer className="footer">
-    {links.map((link, index) => {
-      return <a key={index} href={link.url} target="_blank">
-        <span>{link.title}</span>
-        <i className={link.icon} />
-      </a>
-    })}
-  </footer>
+  return (
+    <footer className="footer">
+      {links.map((link, index) => {
+        return (
+          <a key={index} href={link.url} target="_blank">
+            <span>{link.title}</span>
+            <i className={link.icon} />
+          </a>
+        );
+      })}
+    </footer>
+  );
 }
